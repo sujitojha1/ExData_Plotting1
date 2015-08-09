cd6582b10706d20090dee8fbcb109eece691ca89
sujitojha1 <sujit.ojha@gmail.com>
2015-08-10 00:58:16 +0530

    Time Series

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-# CODE TO PLOT : GLOBAL ACTIVE POWER HISTOGRAM
+# CODE TO PLOT : GLOBAL ACTIVE POWER TIME SERIES
 
 # Reading the data, household_power_consumption.txt
 household_pwr_consum <- read.csv("household_power_consumption.txt",sep = ";",colClasses=c("character","character", rep("numeric",7)),na.strings = "?")
@@ -14,7 +14,10 @@ date_limit_lower <- strptime("01/02/2007 00:00:00","%d/%m/%Y %H:%M:%S")
 good_cases <- c(household_pwr_consum[["Date/Time"]] >= date_limit_lower & household_pwr_consum[["Date/Time"]] < date_limit_upper)
 household_pwr_consum_study <- household_pwr_consum[good_cases,]
 
-#Plotting the histogram
-png(filename = "plot1.png", width=480, height=480)
-hist(household_pwr_consum_study[["Global_active_power"]],main="Global Active Power", xlab="Global Active Power (Kilowatts)", col="RED")
+#Plotting the time series
+png(filename = "plot2.png", width=480, height=480)
+plot(household_pwr_consum_study[["Date/Time"]],household_pwr_consum_study[["Global_active_power"]],type="l",ylab="Global Active Power (Kilowatts)", xlab="")
 dev.off()
+
+#Clearing the workspace
+rm(list = ls())
